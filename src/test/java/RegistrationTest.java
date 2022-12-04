553858a1a8e3fa71cee76d0ff01941516897ac8d
ImtiazSiker <sikderimtiaz@yahoo.com>
2022-12-03 22:52:50 -0500

    Committing Registration Test

---
+++ src/test/java/RegistrationTest.java
@@ -0,0 +1,7 @@
+public class RegistrationTest {
+
+    public static void main(String[] args){
+        System.out.println("Register with Valid Username....");
+        System.out.println("Register with Valid Password....");
+    }
+}
